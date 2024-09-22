5d04309d4cc7861db59eb9fd291792cb2456044b
Pritesh <tinumistry@gmail.com>
2024-09-22 10:10:03 +0530

    first commit

---
+++ src/main/java/com/example/springaiqdrantvectorsearchdemo/SpringaiQdrantVectorSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaiqdrantvectorsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiQdrantVectorSearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiQdrantVectorSearchDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaiqdrantvectorsearchdemo/SpringaiQdrantVectorSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaiqdrantvectorsearchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiQdrantVectorSearchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
